6678ef3f4819b5cc63950a18da972b962f3c7c8c
sriraam <m.sriraam94@gmail.com>
2015-08-25 18:17:16 +0530

    update1 25/8/2015

--- scargame/main.cpp
+++ scargame/main.cpp
@@ -3,7 +3,110 @@
 #include<iostream>
 
 
-using namespace ::std;
+using namespace :: std;
+char repo[3][3];
+
+char winner(){
+	//if (repo[0] == 'x' && repo[1] == 'x' && repo[2] == 'x'){
+		
+//	}
+	if (repo[0][0] == 'x' && repo[0][0] == repo[0][1] && repo[0][0]==repo[0][2]){
+		return 'x';	}//Row 1
+	//if (repo[0][0] == 'x' && repo[0][0] == repo[1][1] && repo[0][0] == repo[2][2]){
+	//	return 'x';//diag l
+	//}
+	if (repo[0][0] == 'x' && repo[0][0] == repo[1][0] && repo[0][0] == repo[2][0]){
+		return 'x';//column 1
+	}
+	if (repo[0][1] == 'x' && repo[0][1] == repo[1][1] && repo[0][1] == repo[2][1]){
+		return 'x';//colum 2
+	}
+	if (repo[0][2] == 'x' && repo[0][2] == repo[1][2] && repo[0][2] == repo[2][2]){
+		return 'x';//column 3
+	}
+	if (repo[1][0] == 'x' && repo[1][0] == repo[1][1] && repo[1][0] == repo[1][2]){
+		return 'x';//row 2
+	}
+	if (repo[2][0] == 'x' && repo[2][0] == repo[2][1] && repo[2][0] == repo[2][2]){
+		return 'x';//row 3
+	}
+	//if (repo[0][2] == 'x' && repo[0][2] == repo[1][1] && repo[0][2] == repo[2][0]){
+	//	return 'x';//diag r
+	//}
+	if (repo[1][1] == 'x' && repo[1][1] == repo[0][0] && repo[1][1] == repo[2][2]){
+		return 'x';//diag r
+	}
+	if (repo[1][1] == 'x' && repo[1][1] == repo[0][2] && repo[1][1] == repo[2][0]){
+		return 'x';//diag r
+	}
+	if (repo[0][0] == 'x' && repo[0][0] == repo[0][1] && repo[0][0]==repo[0][2]){
+		return 'x';	}//Row 1
+	//if (repo[0][0] == 'x' && repo[0][0] == repo[1][1] && repo[0][0] == repo[2][2]){
+	//	return 'x';//diag l
+	//}
+	if (repo[0][0] == 'x' && repo[0][0] == repo[1][0] && repo[0][0] == repo[2][0]){
+		return 'x';//column 1
+	}
+	if (repo[0][1] == 'x' && repo[0][1] == repo[1][1] && repo[0][1] == repo[2][1]){
+		return 'x';//colum 2
+	}
+	if (repo[0][2] == 'x' && repo[0][2] == repo[1][2] && repo[0][2] == repo[2][2]){
+		return 'x';//column 3
+	}
+	if (repo[1][0] == 'x' && repo[1][0] == repo[1][1] && repo[1][0] == repo[1][2]){
+		return 'x';//row 2
+	}
+	if (repo[2][0] == 'x' && repo[2][0] == repo[2][1] && repo[2][0] == repo[2][2]){
+		return 'x';//row 3
+	}
+	//if (repo[0][2] == 'x' && repo[0][2] == repo[1][1] && repo[0][2] == repo[2][0]){
+	//	return 'x';//diag r
+	//}
+	if (repo[1][1] == 'x' && repo[1][1] == repo[0][0] && repo[1][1] == repo[2][2]){
+		return 'x';//diag r
+	}
+	if (repo[1][1] == 'x' && repo[1][1] == repo[0][2] && repo[1][1] == repo[2][0]){
+		return 'x';//diag r
+	}
+
+	// OOOO
+	if (repo[0][0] == 'o' && repo[0][0] == repo[0][1] && repo[0][0] == repo[0][2]){
+		return 'o';
+	}//Row 1
+	//if (repo[0][0] == 'x' && repo[0][0] == repo[1][1] && repo[0][0] == repo[2][2]){
+	//	return 'x';//diag l
+	//}
+	if (repo[0][0] == 'o' && repo[0][0] == repo[1][0] && repo[0][0] == repo[2][0]){
+		return 'o';//column 1
+	}
+	if (repo[0][1] == 'o' && repo[0][1] == repo[1][1] && repo[0][1] == repo[2][1]){
+		return 'o';//colum 2
+	}
+	if (repo[0][2] == 'o' && repo[0][2] == repo[1][2] && repo[0][2] == repo[2][2]){
+		return 'o';//column 3
+	}
+	if (repo[1][0] == 'o' && repo[1][0] == repo[1][1] && repo[1][0] == repo[1][2]){
+		return 'o';//row 2
+	}
+	if (repo[2][0] == 'o' && repo[2][0] == repo[2][1] && repo[2][0] == repo[2][2]){
+		return 'o';//row 3
+	}
+	//if (repo[0][2] == 'x' && repo[0][2] == repo[1][1] && repo[0][2] == repo[2][0]){
+	//	return 'x';//diag r
+	//}
+	if (repo[1][1] == 'o' && repo[1][1] == repo[0][0] && repo[1][1] == repo[2][2]){
+		return 'o';//diag r
+	}
+	if (repo[1][1] == 'o' && repo[1][1] == repo[0][2] && repo[1][1] == repo[2][0]){
+		return 'o';//diag r
+	}
+
+}
+	
+
+
+	
+
 int main(){
 
 	sf::RenderWindow window;
@@ -15,12 +118,14 @@ int main(){
 	sf::Texture textureo;
 	sf::Texture texturex;
 	sf::Vector2i m_pos;
-	sf::Sprite selected[8];
+	sf::Sprite selected[9];
 	sf::Sprite sxo;
-	int turn = 1; 
+	
+	char play;
+	int turn = 1,i; 
 
 	bool start = false;
-
+	bool win = false;
 	sf::Font font;
 	font.loadFromFile("Transformers Movie.ttf");
 
@@ -59,18 +164,18 @@ int main(){
 	x.setTexture(texturex);
 	o.setTexture(textureo);
 
-	
 
-	
 
-	
+
 	while (window.isOpen()){
 		if (turn % 2 == 0){
 			
 			sxo = o;
+			play = 'o';
 		}
 		else{
 			sxo = x;
+			play = 'x';
 		}
 
 		/*if (start == false){
@@ -96,6 +201,19 @@ int main(){
 				start = true;
 
 				std::cout << "Mousex" << m_pos.x << " Mousey" << m_pos.y << std::endl;
+
+
+
+				/*
+               THE BELOW if statements are to divide the coordinates of the selected region
+			   Grid[]--> boolean opearion ,which avoids the repeated selection of region
+			   Turn  --> at each turn x & o changes alternatively.
+			             it is used to swap the x and o display using even oper   
+               selected[a]-->It is a sprite,which holds the X and O image of selected region.
+			                 a-> depends upon selected region,
+							 it stores x & O alternatively using turn condition at initial
+               repo[]-->same like selected,instead of image it contains char for validating the result
+				*/
 				if (m_pos.x > 1 && m_pos.x <= 100 && m_pos.y > 100 && m_pos.y < 200){
 
 					if (grid[0] == false){
@@ -105,7 +223,7 @@ int main(){
 						turn++;
 						selected[0] = sxo;
 						selected[0].setPosition(25, 115);
-						
+						repo[0][0] = play;
 					}
 				}
 				if (m_pos.x > 100 && m_pos.x <= 200 && m_pos.y > 100 && m_pos.y < 200){
@@ -116,7 +234,7 @@ int main(){
 						turn++;
 						selected[1] = sxo;
 						selected[1].setPosition(120, 115);
-						
+						repo[0][1] = play;
 					}
 				}
 				if (m_pos.x > 200 && m_pos.x <= 300 && m_pos.y > 100 && m_pos.y <= 200){
@@ -127,7 +245,7 @@ int main(){
 						turn++;
 						selected[2] = sxo;
 						selected[2].setPosition(220, 115);
-						
+						repo[0][2] = play;
 					}
 				}
 				if (m_pos.x > 1 && m_pos.x <= 100 && m_pos.y > 200 && m_pos.y <= 300){
@@ -138,7 +256,7 @@ int main(){
 						turn++;
 						selected[3] = sxo;
 						selected[3].setPosition(25, 215);
-						
+						repo[1][0] = play;
 					}
 				}
 				if (m_pos.x > 100 && m_pos.x <= 200 && m_pos.y > 200 && m_pos.y <= 300){
@@ -149,7 +267,7 @@ int main(){
 						turn++;
 						selected[4] = sxo;
 						selected[4].setPosition(120, 215);
-						
+						repo[1][1] = play;
 					  }
 				}
 				if (m_pos.x > 200 && m_pos.x <= 300 && m_pos.y > 200 && m_pos.y <= 300){
@@ -160,7 +278,7 @@ int main(){
 						turn++;
 						selected[5] = sxo;
 						selected[5].setPosition(220, 215);
-					
+						repo[1][2] = play;
 					}
 				}
 				if (m_pos.x > 1 && m_pos.x <= 100 && m_pos.y > 300 && m_pos.y <= 400){
@@ -171,7 +289,7 @@ int main(){
 						turn++;
 						selected[6] = sxo;
 						selected[6].setPosition(25, 315);
-						
+						repo[2][0] = play;
 					}
 				}
 				if (m_pos.x > 100 && m_pos.x <= 200 && m_pos.y > 300 && m_pos.y <= 400){
@@ -182,7 +300,7 @@ int main(){
 						turn++;
 						selected[7] = sxo;
 						selected[7].setPosition(120, 315);
-						
+						repo[2][1] = play;
 					}
 				}
 				if (m_pos.x > 200 && m_pos.x <= 300 && m_pos.y > 300 && m_pos.y <= 400){
@@ -193,15 +311,38 @@ int main(){
 						turn++;
 						selected[8] = sxo;
 						selected[8].setPosition(220, 315);
-						
+						repo[2][2] = play;
 					}
 				}
 
 			}
 		}
 
+		if (win == false){
+			if (winner() == 'x'){
+				win = true;
+				cout << "THE WINNER IS X";
+
+			}
+			if (winner() == 'o'){
+				win = true;
+				cout << "THE WINNER IS O";
+
+			}
+		}
+
+		if (grid[4] == true){
+			for (i = 0; i < 9; i++){
+				cout << repo[i]<<"\n";
+				
+			}
+			grid[4] = false;
+		}
 		window.draw(sprite);
-		window.draw(selected[2]);
+		for (i = 0; i < 9; i++){
+			window.draw(selected[i]);
+		}
+		
 		window.display();
 	}
 
